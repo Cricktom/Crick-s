b5525a4befa4a09ad3782b386b479621b89b426e
Cricktom <nitintomar332@gmail.com>
2017-04-07 19:16:57 +0530

    first commit

---
+++ Election/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import  url
+from . import views
+
+urlpatterns = [
+    # Examples:
+        # url(r'^escalations/analytics/get$', escalations.get_analytics, name='escalations_analytics'),
+    # url(r'^$', 'Election_portal.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+
+]
